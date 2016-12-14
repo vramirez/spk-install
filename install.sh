33e3ae6b1261466e5f8175dfe88a846269e353ba
Victor Ramirez <ramirez.vmanuel@gmail.com>
2016-12-14 23:36:49 +0000

    Installer v0.5

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+wget http://apache.uniminuto.edu/spark/spark-2.0.2/spark-2.0.2-bin-hadoop2.7.tgz 
