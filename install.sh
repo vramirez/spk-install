482d815da37b911445cdb047ee9368009038f070
Victor Ramirez <ramirez.vmanuel@gmail.com>
2016-12-14 23:58:34 +0000

    Silent JDK install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install oracle-java8-installer -y
 wget http://mirrors.gigenet.com/apache/spark/spark-2.0.2/spark-2.0.2-bin-hadoop2.7.tgz
